c67a2d7c665b159115192d7f04bdc00ba2ca708c
Oleh Tsipotan <tsipotan.oleh@gmail.com>
2024-02-09 23:06:48 +0200

    update GlobalExceptionHandler

--- src/main/java/com/service/order/handler/GlobalExceptionHandler.java
+++ src/main/java/com/service/order/handler/GlobalExceptionHandler.java
@@ -5,16 +5,16 @@ import com.service.order.exception.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
 @Slf4j
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_GATEWAY)
